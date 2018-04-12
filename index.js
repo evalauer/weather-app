d89902e01736c6d7ca583251994810a0e0a656bf
evalauer <eva.r.lauer@gmail.com>
2018-04-12 19:04:48 -0400

    Final!

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-var userEntry= 
-{parseInt(document.getElementById(“fieldname”).value);
-} 
-
-var new= (userEntry * 1.8) + 32;
+var userEntry;
+var newtemp;
 
 
-document.getElementById("sumbit").onclick=function() {
-	new = document.getElementById(".value").innerHTML;
+document.getElementById("submit").onclick=function() {
+	userEntry = parseInt(document.getElementById("fieldName").value);
+	newtemp = (userEntry * 1.8) + 32;
+	console.log(userEntry, newtemp);
+	document.getElementById("far").innerHTML=userEntry;
+	document.getElementById("cel").innerHTML=newtemp;
+
+	if (newtemp < 32) { 
+	document.body.style.backgroundColor= "#00F";
+	}
 
 
-if (new < 32) { 
-document.body.style.backgroundColor= "00F";
-}
+	if (newtemp >= 32 && newtemp <= 50) { 
+	document.body.style.backgroundColor= "#FFa500";
+	}
 
+	if (newtemp > 50 && newtemp <= 70) { 
+	document.body.style.backgroundColor= "#FFFF00";
+	}
 
-if (new > = 32 && new < = 50) { 
-document.body.style.backgroundColor= "FFa500";
-}
-
-if (new > 50 && new < = 70) { 
-document.body.style.backgroundColor= "FFFF00";
-}
-
-if (new > 70) { 
-document.body.style.backgroundColor= "FF0000";
+	if (newtemp > 70) { 
+	document.body.style.backgroundColor= "#FF0000";
+	}
 }
 
 
